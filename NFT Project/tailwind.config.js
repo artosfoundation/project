f3b9c964ae233a696f4719d297f7a10e3c4b1c43
Artos Token <96391739+artosfoundation@users.noreply.github.com>
2022-06-12 18:21:27 +0700

    Add files via upload

---
+++ NFT Project/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        'cairo': ['Cairo']
+      },
+      zIndex: {
+        '100': '100',
+      }
+    },
+  },
+  plugins: [],
+}
